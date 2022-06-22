b4df6ef4e5c3769c8a88c01b5509dc14fbd89378
Максим Игнатов <ignatov.mack@gmail.com>
2022-06-22 23:53:55 +0300

    Add Follow serializers

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,17 @@
-from django.urls import path
+from api import views
+from django.urls import include, path
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'posts', views.PostViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r"posts/(?P<post_id>\d+)/comments",
+                views.CommentsViewSet,
+                basename='comments'
+                )
+router.register(r'follow', views.FollowViewSet, basename='follow')
+
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,53 @@
-# TODO:  Напишите свой вариант
+from rest_framework.exceptions import ValidationError
+
+from posts.models import Group, Post, Follow
+from rest_framework import permissions, viewsets
+from rest_framework.generics import get_object_or_404
+
+from .permission import UserIdentificationObject
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer, FollowSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (
+        permissions.IsAuthenticated,
+        UserIdentificationObject
+    )
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentsViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (
+        permissions.IsAuthenticated,
+        UserIdentificationObject
+    )
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        return post.comments.all()
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        return serializer.save(author=self.request.user, post=post)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        follow = Follow.objects.filter(user=self.request.user)
+        return follow
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
